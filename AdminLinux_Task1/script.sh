bbbd8fc54a6a00696e444e4d23df735cc75f3cd2
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2024-11-30 17:09:45 +0200

    Task1 Admin Linux

---
+++ AdminLinux_Task1/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir ~/myDirectory ~/myDirectory/secondDirectory
+touch ~/myDirectory/secondDirectory/myNotePaper
+cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory
+mv ~/myDirectory/myNotePaper ~/myDirectory/myOldNotePaper
+
